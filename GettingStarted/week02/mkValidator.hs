dff425e0cb5581673abd8336e59cce7639d1f574
Eric Coetzee <36683674+EricCoetzee@users.noreply.github.com>
2021-05-11 19:20:01 +0200

    Update mkValidator.hs

--- GettingStarted/week02/mkValidator.hs
+++ GettingStarted/week02/mkValidator.hs
@@ -33,3 +33,10 @@ mkValidator _ _ _ = ()
 
 validator :: Validator
 validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash validator
+
+scrAddress :: Ledger.Address
+scrAddress = ScriptAddress valHash
+
